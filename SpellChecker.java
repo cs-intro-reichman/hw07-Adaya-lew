d1ca28addbf93a12f1df63baabb7f935e46f6194
Adaya-lew <adaya.lewartowski@post.runi.ac.il>
2024-02-02 14:00:03 +0200

    hw07

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,4 +1,3 @@
-
 public class SpellChecker {
 
 
@@ -17,29 +16,23 @@ public class SpellChecker {
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		word1 = word1.toLowerCase();
-		word2 = word2.toLowerCase();
-		int l1 = word1.length();
-		int l2 = word2.length();
-		if (l2 == 0)
-		{
-            return l1;
-        }
-		else if (l1 == 0)
-		{
-			return l2;
-		} 
-		else if (word1.charAt(0) == word2.charAt(0))
-		{
-			return levenshtein(tail(word1),tail(word2));
-		}
-		else
-		{
-			return 1+minimal(levenshtein(tail(word1),word2),levenshtein(word1,tail(word2)),levenshtein(tail(word1),tail(word2)));
-		}
-		
-	}
+		String lowerC1 = word1.toLowerCase();  //make it to toLowerCase lethers
+		String lowerC2 = word2.toLowerCase();
 
+		int lword1 = word1.length();
+		int lword2 = word2.length();
+
+		if(lword2 == 0)
+			return lword1;
+		if(lword1 == 0)
+			return lword2;
+		if (word1.charAt(0) == word2.charAt(0))
+			return levenshtein(tail(word1), tail(word2));//Otherwise try all three possible actions and select the best one
+		else {
+			return 1 + Math.min(Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2))), levenshtein(tail(word1), tail(word2)));
+		}
+
+	}
 
 
 
@@ -49,41 +42,29 @@ public class SpellChecker {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-		for (int i = 0; i < dictionary.length; i++) {
-				dictionary[i] = in.readString();
-			}
-		return dictionary;
-	}
+
+        for (int i = 0; i < dictionary.length; i++) {
+            dictionary[i] = in.readLine();
+        }
+
+        return dictionary;
+    }
 
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		int minimal = levenshtein(word,dictionary[0]);
-		int index = 0;
-		int lv = 0;
-		if (dictionary[0].equals(word))
-		{
-			return dictionary[0];
+		int min = word.length();
+		String minimal = word;
+		for(int i = 0; i < dictionary.length; i++){
+			int distance = levenshtein(word, dictionary[i]);
+			if(distance <= threshold){
+			 	if(distance < min){
+				min = distance;
+				minimal = dictionary[i];
+				}
+			}
 		}
-		else 
-			{
-			for ( int i = 1; i < dictionary.length; i++)
-		        {
-			      lv = levenshtein(word,dictionary[i]);
-			      if (lv < minimal) 
-			       {
-				       minimal = lv;
-				       index = i;
-			        }
-		         }
-		         if (minimal > threshold)
-		         {
-		         	return word;
-		         }
-		         else 
-		         {
-		         	return dictionary[index];
-		         }
-
-		}
+		return minimal;
 	}
+	
+
 }
