5ed2f212ee73d8f772d7e04649e8941b542786ca
Adaya-lew <adaya.lewartowski@post.runi.ac.il>
2024-02-02 14:21:56 +0200

    hw07

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,28 +11,29 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-		for (int i = 0; i < 3000; i++) {
-				dictionary[i] = in.readString();
-			}
-		return dictionary;
+
+		// Your code here
+			for (int i = 0; i < 3000; i++) {                       //go over the 3000 words string
+				dictionary [i] = in.readString();                  //make dictionary an array that is all the string (readString read all the doc of words)
+    		}
+		return dictionary;                                        //returns the dictionary in array form
 	}
 
-		
-
-	
-
 	public static boolean existInDictionary(String word, String []dictionary) {
-		for (int i = 0; i < dictionary.length; i++) {                 
-			if (dictionary[i].equals(word)){                                   
+		// Your code here
+
+		for (int i = 0; i < dictionary.length; i++) {                 //go over the words in the dictionary (the dictionary contains words (doent read per letter))
+			if (dictionary[i].equals(word)){                                   //if the word in index i is the same as the word we got return true, then goes to the next word i++
 			return true;
 			}
 		}
-		return false;
+	return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
 
-		if (hashtag.isEmpty()) {
+		// Base case: do nothing (return) if hashtag is an empty string.
+        if (hashtag.isEmpty()) {
             return;
         }
 
@@ -41,11 +42,11 @@ public class HashTagTokenizer {
 
         for (int i = 1; i <= N; i++) {
 		
-			if (existInDictionary(lowCaseHash.substring(0, i), dictionary)){               
-				System.out.println(lowCaseHash.substring(0, i));                           
+			if (existInDictionary(lowCaseHash.substring(0, i), dictionary)){               //calls the previos function to check if the letters between 0 and i are a word in the dictionary
+				System.out.println(lowCaseHash.substring(0, i));                           //if it is a word, print it, if not continues to i++
 
-				breakHashTag(lowCaseHash.substring(i, N), dictionary);                                
-				                                                                                    
+				breakHashTag(lowCaseHash.substring(i, N), dictionary);                                //if it was a word make the new variables we recieve in the current function to be form the index finishing
+				                                                                                    //the word to the end of the string, and the dictionary and make the check again
 				return;
        		 }
    		 }
